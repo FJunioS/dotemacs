82c2efacd0b9ff6bd9cee22aa49adebce7e36dd3
Junio Santos <git@junio.dev>
2023-12-23 15:49:52 -0300

    Add Operation System's related keymaps

--- layers/OS.el
+++ layers/OS.el
@@ -2,7 +2,33 @@
 ;;; Commentary:
 ;;; Code:
 
-;;;; NixOS
+;; Linux
+(defun ju/system-sleep ()
+  (interactive)
+  (save-all-history)
+  (call-process "systemctl" nil nil nil "suspend"))
+(defalias 'ju/system-suspend 'ju/system-sleep)
+
+(defun ju/keymap-dvorak ()
+  (interactive)
+  (call-process "setxkbmap" nil nil nil "-variant" "dvp"))
+
+(defun ju/keymap-qwerty ()
+  (interactive)
+  (call-process "setxkbmap" nil nil nil "-variant" ","))
+
+(defun ju/system-shutdown ()
+  (interactive)
+  (save-all-history)
+  (call-process "systemctl" nil nil nil "poweroff"))
+
+(defun ju/system-logout ()
+  (interactive)
+  (save-all-history)
+  (call-process "pkill" nil nil nil
+                "-KILL" "-u" (user-login-name)))
+
+;; NixOS
 (defun nixos-rebuild ()
   "Rebuild the nixos system."
   (interactive)
